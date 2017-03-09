23a3251ce970f04558b3cabb34980ad25315d47b
javedshaik786 <javed.shaik6@gmail.com>
2017-03-09 11:20:01 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,11 +19,11 @@
 -include device/samsung/smdk4412-common/BoardCommonConfig.mk
 -include device/samsung/smdk4412-qcom-common/BoardCommonConfig.mk
 
--include device/samsung/t0lte/BoardCommonConfig.mk
+-include device/samsung/ms013g/BoardCommonConfig.mk
 
 # GPS
 BOARD_GPS_SET_PRIVACY := true
 
 # inherit from the proprietary version
--include vendor/samsung/t0lte/BoardConfigVendor.mk
+-include vendor/samsung/ms013g/BoardConfigVendor.mk
 
