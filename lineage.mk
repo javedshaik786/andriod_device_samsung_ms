1ec987874029e1438aef20117c715bbe7356b2a9
javedshaik786 <javed.shaik6@gmail.com>
2017-03-09 11:00:27 +0530

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -1,22 +1,22 @@
 # Release name
-PRODUCT_RELEASE_NAME := t0lte
+PRODUCT_RELEASE_NAME := ms013g
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/t0lte/full_t0lte.mk)
+$(call inherit-product, device/samsung/ms013g/full_ms013g.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := t0lte
-PRODUCT_NAME := lineage_t0lte
+PRODUCT_DEVICE := ms013g
+PRODUCT_NAME := lineage_ms013g
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := GT-N7105
+PRODUCT_MODEL := GT-G7102
 PRODUCT_MANUFACTURER := samsung
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t0lte TARGET_DEVICE=t0lte BUILD_FINGERPRINT="samsung/t0ltexx/t0lte:4.4.2/KOT49H/N7105XXUFOB2:user/release-keys" PRIVATE_BUILD_DESC="t0ltexx-user 4.4.2 KOT49H N7105XXUFOB2 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ms013g TARGET_DEVICE=ms013g BUILD_FINGERPRINT="samsung/ms013g/ms013g:4.4.2/KOT49H/G7102DDUBNK1:user/release-keys" PRIVATE_BUILD_DESC="ms013g-user 4.4.2 KOT49H G7102DDUBNK1 release-keys"
